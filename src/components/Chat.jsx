9c5bec2c81a2a48b7c9ab5589852af458816c55e
durgesh14 <34915741+durgesh14@users.noreply.github.com>
2023-03-27 01:14:25 +0530

    added onclick on user chat

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import Input from "../components/Input";
 import Messages from "../components/Messages";
 import profile from "../assets/profile.jpg";
+import { ChatContext } from "../context/ChatContext";
 
 const Chat = () => {
+  const { data } = useContext(ChatContext);
   return (
     <div className="chat">
       <div className="chatInfo">
-        <span>Jane</span>
+        <span>{data.user?.displayName}</span>
         <div className="chatIcons">
           <img src={profile} alt="" />
           <img src={profile} alt="" />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   const { currentUser } = useContext(AuthContext);
   return (
     <div className="navbar">
-      <span className="logo">Lama Chat</span>
+      <span className="logo">React Chat</span>
       <div className="user">
         <img src={currentUser.photoURL} alt="" />
         <span>{currentUser.displayName}</span>
