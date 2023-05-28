f312e249df5b1fa5cf7318c2b41a91883bd5d8f1
durgesh14 <34915741+durgesh14@users.noreply.github.com>
2023-05-28 21:54:39 +0530

    Added api keys to .env

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
-
 // const firebaseConfig = {
 //   apiKey: "AIzaSyAbMZjEAWMDskL4HFOuCCCmxjY5pCiTnGQ",
 //   authDomain: "react-chat-app-9e034.firebaseapp.com",
@@ -14,14 +13,15 @@ import { getFirestore } from "firebase/firestore";
 //   measurementId: "G-M1JPWRJ87T",
 // };
 const firebaseConfig = {
-  apiKey: "AIzaSyAbMZjEAWMDskL4HFOuCCCmxjY5pCiTnGQ",
-  authDomain: "react-chat-app-9e034.firebaseapp.com",
-  projectId: "react-chat-app-9e034",
-  storageBucket: "react-chat-app-9e034.appspot.com",
-  messagingSenderId: "336495715651",
-  appId: "1:336495715651:web:6ba24dd7455f10e0a0fbf6",
-  measurementId: "G-M1JPWRJ87T",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
+
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
